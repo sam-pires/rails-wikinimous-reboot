7f718c1cbb61ebf75265e8a36bef7dcadade89bb
Samuel pires <samuel.pires.pro@gmail.com>
2019-01-02 16:12:52 +0100

    halfway done

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,24 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end

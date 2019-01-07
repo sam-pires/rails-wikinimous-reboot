b5fcb838f3d0055d0fb179f0f9df1e638f0c5c26
Samuel pires <samuel.pires.pro@gmail.com>
2019-01-07 21:36:54 +0100

    done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,17 +8,38 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to articles_path(@article)
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    redirect_to articles_path(@article)
   end
 
-  def delete
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
 end
